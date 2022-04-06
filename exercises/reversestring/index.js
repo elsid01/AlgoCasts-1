a43066b6d2e336ce22df51536efe0f73dfad4314
elsid01 <ely_mbaye@yahoo.com>
2022-04-05 22:41:12 -0700

    april 5th commit2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   let word = '';
+//   for(let i = 0; i < str.length ; i++){
+//     word += str[ str.length - (i + 1)] ;
+//   }
+//   return word;
+// }
 
+// function reverse(str){
+// return  str.split('').reduce((l1, l2) => l2 + l1, '')
+// }
+
+// function reverse(str){
+//   return  str.split('').reverse().join('');
+//   }
+
+function reverse(str){
+  let reversed = '';
+  for(let char of str){
+    debugger;
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+reverse('alpha');
 module.exports = reverse;
