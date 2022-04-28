0b43cc421dbe1c17e9ee53d1424a5832767a60fd
elsid01 <ely_mbaye@yahoo.com>
2022-04-28 01:19:32 -0700

    clear method implementation

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -17,6 +17,30 @@ class LinkedList {
   insertFirst(data){
    this.head = new Node(data, this.head);
   }
+
+  size(){
+    let counter = 0;
+    while(this.head){
+      counter++;
+      this.head = this.head.next;
+    }
+    return counter;
+  }
+
+  getFirst(){
+    return this.head;
+  }
+
+  getLast(){
+     while(this.head){
+       if(!this.head.next) return this.head;
+       this.head = this.head.next;
+     }
+  }
+
+  clear(){
+      this.head = null;
+  }
 }
 
 module.exports = { Node, LinkedList };
